45783c688cbe851ec91355a3b96bd9481eb80840
raul <74162376+raulanto@users.noreply.github.com>
2024-03-09 23:43:28 -0600

    init

---
+++ src/views/Tab1Page.vue
@@ -0,0 +1,56 @@
+<template>
+  <ion-page>
+    <ion-header>
+      <ion-toolbar>
+        <ion-title>Grid</ion-title>
+      </ion-toolbar>
+    </ion-header>
+    <ion-content :fullscreen="true">
+      <b>Column 2 has <code>size</code> set to <code>"8"</code></b>
+      <ion-grid>
+        <ion-row>
+          <ion-col>1</ion-col>
+          <ion-col size="8">2</ion-col>
+          <ion-col>3</ion-col>
+        </ion-row>
+      </ion-grid>
+    
+      <b>Columns 3 & 4 have <code>size</code> set to <code>"3"</code></b>
+      <ion-grid>
+        <ion-row>
+          <ion-col>1</ion-col>
+          <ion-col>2</ion-col>
+          <ion-col size="3">3</ion-col>
+          <ion-col size="3">4</ion-col>
+          <ion-col>5</ion-col>
+          <ion-col>6</ion-col>
+        </ion-row>
+      </ion-grid>
+    
+      <b>Columns 1 & 2 have <code>size</code> set to <code>"4"</code></b>
+      <ion-grid>
+        <ion-row>
+          <ion-col size="4">1</ion-col>
+          <ion-col size="4">2</ion-col>
+          
+        </ion-row>
+      </ion-grid>
+
+    </ion-content>
+  </ion-page>
+</template>
+
+<script setup lang="ts">
+import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent,IonGrid,IonRow,IonCol } from '@ionic/vue';
+// import ExploreContainer from '@/components/ExploreContainer.vue';
+
+</script>
+
+<style>
+ion-col {
+  background-color: #135d54;
+  border: solid 1px #fff;
+  color: #fff;
+  text-align: center;
+}
+</style>
