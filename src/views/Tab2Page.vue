3970ab60120b65c3e4d3d72a09b3db96bfc0a73f
raul <74162376+raulanto@users.noreply.github.com>
2024-03-11 11:30:37 -0600

    tailwind

--- src/views/Tab2Page.vue
+++ src/views/Tab2Page.vue
@@ -6,18 +6,15 @@
       </ion-toolbar>
     </ion-header>
     <ion-content :fullscreen="true">
-      <ion-header collapse="condense">
-        <ion-toolbar>
-          <ion-title size="large">Tab 2</ion-title>
-        </ion-toolbar>
-      </ion-header>
+      <section class="flex justify-between">
+        <p class="font-bold text-base">IONIC</p>
+        <p class="text-base">TAILWINDCSS</p>
+      </section >
 
-      <ExploreContainer name="Tab 2 page" />
     </ion-content>
   </ion-page>
 </template>
 
 <script setup lang="ts">
 import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/vue';
-import ExploreContainer from '@/components/ExploreContainer.vue';
 </script>
